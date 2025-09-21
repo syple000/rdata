c19906c595ba47b3688edcd6f2f619ecd406da0d
syple000 <1519761174@qq.com>
2025-09-21 12:36:22 +0800

    更新时间方法

--- time/src/time.rs
+++ time/src/time.rs
@@ -4,3 +4,8 @@ pub fn get_current_nano_timestamp() -> u128 {
     let now = SystemTime::now();
     now.duration_since(UNIX_EPOCH).unwrap().as_nanos()
 }
+
+pub fn get_current_milli_timestamp() -> u128 {
+    let now = SystemTime::now();
+    now.duration_since(UNIX_EPOCH).unwrap().as_millis()
+}
