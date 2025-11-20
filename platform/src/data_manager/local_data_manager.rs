6b49c11177e27ac927ea117b244c3a4a04b8a413
syple000 <1519761174@qq.com>
2025-11-20 23:31:47 +0800

    更新local data manager

--- platform/src/data_manager/local_data_manager.rs
+++ platform/src/data_manager/local_data_manager.rs
@@ -1,3 +1,9 @@
+use crate::{
+    config::PlatformConfig,
+    data_manager::{db::*, MarketDataManager},
+    errors::{PlatformError, Result},
+    models::{DepthData, KlineData, KlineInterval, MarketType, SymbolInfo, Ticker24hr, Trade},
+};
 use async_trait::async_trait;
 use db::sqlite::SQLiteDB;
 use std::{
@@ -8,13 +14,6 @@ use std::{
     },
 };
 use tokio::sync::RwLock;
-
-use crate::{
-    config::PlatformConfig,
-    data_manager::MarketDataManager,
-    errors::Result,
-    models::{DepthData, KlineData, KlineInterval, MarketType, SymbolInfo, Ticker24hr, Trade},
-};
 
 pub struct Clock {
     cur_ts: AtomicU64, // 毫秒时间戳
@@ -37,12 +36,9 @@ impl Clock {
 }
 
 pub struct LocalMarketDataManager {
-    config: Arc<PlatformConfig>,
-
     clock: Arc<Clock>,
 
     db: Arc<SQLiteDB>,
-    load_batch_size: usize,
     max_cache_size: usize,
 
     klines: Arc<HashMap<(MarketType, String, KlineInterval), Arc<RwLock<VecDeque<KlineData>>>>>,
@@ -54,7 +50,6 @@ impl LocalMarketDataManager {
         config: Arc<PlatformConfig>,
         clock: Arc<Clock>,
         db: Arc<SQLiteDB>,
-        load_batch_size: usize,
         max_cache_size: usize,
     ) -> Self {
         let mut klines = HashMap::new();
@@ -76,21 +71,96 @@ impl LocalMarketDataManager {
         }
 
         Self {
-            config,
             clock,
             db,
-            load_batch_size,
             max_cache_size,
             klines: Arc::new(klines),
             trades: Arc::new(trades),
         }
+    }
+
+    async fn load_klines(
+        &self,
+        market_type: &MarketType,
+        symbol: &String,
+        interval: &KlineInterval,
+    ) -> Result<()> {
+        let cur_ts = self.clock.cur_ts();
+
+        let cache = match self
+            .klines
+            .get(&(market_type.clone(), symbol.clone(), interval.clone()))
+        {
+            None => {
+                return Err(PlatformError::PlatformError {
+                    message: format!(
+                        "kline cache not found for {:?}, {}, {:?}",
+                        market_type, symbol, interval
+                    ),
+                });
+            }
+            Some(cache) => cache,
+        };
+
+        let mut klines = cache.write().await;
+
+        loop {
+            if !klines.is_empty() && klines.back().unwrap().close_time > cur_ts {
+                return Ok(());
+            }
+            let start_time = if !klines.is_empty() {
+                Some(klines.back().unwrap().close_time + 1)
+            } else {
+                None
+            };
+            let end_time = if start_time.is_none() {
+                Some(cur_ts)
+            } else {
+                None
+            };
+            let db_klines = get_klines(
+                self.db.clone(),
+                market_type,
+                symbol,
+                interval,
+                start_time,
+                end_time,
+                None,
+            )
+            .map_err(|e| PlatformError::PlatformError {
+                message: format!("get klines db err: {}", e),
+            })?;
+
+            if db_klines.is_empty() {
+                return Ok(());
+            }
+
+            for kline in db_klines.iter() {
+                if klines.len() > self.max_cache_size {
+                    klines.pop_front();
+                }
+                klines.push_back(kline.clone());
+            }
+        }
+    }
+
+    async fn load_trades(&self, market_type: &MarketType, symbol: &String) -> Result<()> {
+        todo!()
     }
 }
 
 #[async_trait]
 impl MarketDataManager for LocalMarketDataManager {
     async fn init(&self) -> Result<()> {
-        todo!()
+        for (market_type, symbol, interval) in self.klines.keys() {
+            self.load_klines(market_type, symbol, interval).await?;
+        }
+
+        for (market_type, symbol) in self.trades.keys() {
+            self.load_trades(market_type, symbol).await?;
+        }
+
+        Ok(())
     }
 
     async fn get_klines(
